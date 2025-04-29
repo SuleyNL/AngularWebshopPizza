1bf6c644cb2b6ff47d42742b5ee05a65464bce4f
suley <s1115672@student.hsleiden.nl>
2025-04-29 22:16:39 +0200

    CHANGE ENVIRONMENT.TS

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,9 @@
+// export const environment = {
+//   production: false,
+//   apiUrl: 'http://localhost:8080'
+// }; 
+
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8080'
+  production: true,
+  apiUrl: 'https://angularwebshoppizza-production.up.railway.app/api' // API URL relative to the current host - will be handled by nginx
 }; 
