ca8ef94ee5c029c40416795e80fe58ad1b331022
suley <s1115672@student.hsleiden.nl>
2025-04-29 23:46:05 +0200

    removed cors on login

--- backend/src/main/java/com/pizza/controller/AuthController.java
+++ backend/src/main/java/com/pizza/controller/AuthController.java
@@ -23,7 +23,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "http://localhost:4200")
 public class AuthController {
     private static final Logger LOGGER = LoggerFactory.getLogger(AuthController.class);
 
